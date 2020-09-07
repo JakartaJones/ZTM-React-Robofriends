0ae19a8c19c9b7dc134e69510dead6a2812b5230
JakartaJones <mattjones7@gmail.com>
2020-09-07 16:22:24 -0600

    Updates

---
+++ precache-manifest.ad06595b0596c0f4d000f498056dcdca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbf5559b04686bce94b1abc3067c4da",
+    "url": "/ZTM-React-Robofriends/index.html"
+  },
+  {
+    "revision": "c1dddf4b06ba819b6180",
+    "url": "/ZTM-React-Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "86ce4249d047b0b26f89",
+    "url": "/ZTM-React-Robofriends/static/css/main.09d52cd5.chunk.css"
+  },
+  {
+    "revision": "c1dddf4b06ba819b6180",
+    "url": "/ZTM-React-Robofriends/static/js/2.76453558.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ZTM-React-Robofriends/static/js/2.76453558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ce4249d047b0b26f89",
+    "url": "/ZTM-React-Robofriends/static/js/main.2e5dd57c.chunk.js"
+  },
+  {
+    "revision": "11a441e92db017212dec",
+    "url": "/ZTM-React-Robofriends/static/js/runtime-main.d8502c9d.js"
+  }
+]);
